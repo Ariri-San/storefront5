25aa44c639a18ce118378bb01632a841e2c6acfe
Shayan <shayanghodos@gmail.com>
2023-08-03 09:57:48 +0330

    template 2

--- storefront/urls.py
+++ storefront/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
-    path('store/', include('store.urls')),
+    path('store/', include('store.urls'), name='store'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls)),
